d858ded94a0383d3026e875bd77ff7b7b3af3436
sfah007 <69042482+sfah007@users.noreply.github.com>
2021-03-03 19:06:31 +0300

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -6,7 +6,7 @@ class Config(Config):
     TG_BOT_TOKEN= "1640796806:AAEVKLVqOVK98wsnUT440SoEFXtsWtyjohQ" #imp
     APP_ID = 2180427 #imp
     API_HASH = "b52730e42aabdb05615832d48c702ce6" #imp
-    AUTH_CHANNEL = [-100138716715, 201839212] #imp replace your_id with your id from telegram or delete
+    AUTH_CHANNEL = [-1001435264443, 201839212] #imp replace your_id with your id from telegram or delete
     INDEX_LINK = "https://torrentleech.torrentleech-gdrive.workers.dev
     GLEECH_COMMAND = "gleech"
     YTDL_COMMAND = 'ytdl'
